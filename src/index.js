f84b41461d6c9cbd589dfed72813a09dd49530ce
Expensiveee <TheExpensiveee@gmail.com>
2022-06-30 21:01:42 +0100

    ✨ Add Client class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const { Client } = require('./classes/Client');
+const config = require('dotenv').config().parsed;
+
+const token = config.BOT_TOKEN;
+const prefix = config.PREFIX;
+
+const client = new Client(token, prefix);
+client.connect();
