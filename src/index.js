70dcf9f9e13f6944cd60d4f38dfd8ee7867f0094
Derpinou <lightcompte@gmail.com>
2022-07-04 21:19:45 +0200

    :rocket: Update to Extended Client

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 const { GatewayIntentBits, GatewayDispatchEvents } = require('discord-api-types/v10');
-const { Client } = require('./Core/Client');
+const Client = require('./Bot/Classes/Client');
 const { Guilds, GuildMembers, MessageContent, GuildMessages } = GatewayIntentBits;
 const { Ready, GuildCreate, GuildDelete, MessageCreate, GuildUpdate, InteractionCreate, MessageReactionAdd, MessageReactionRemove, PresenceUpdate, VoiceStateUpdate } = GatewayDispatchEvents;
 const config = require('dotenv').config().parsed;
 
-const client = new Client({
+new Client({
 	token: config.TOKEN,
 	firstShardId: 0,
-	lastShardId: 1,
-	shardCount: 2,
-	autoreconnect: true,
-	/*
-	presence: {
-		activities: [
-			{
-				name: 'Adya-core',
-				type: 0,
-			},
-		],
-		status: 'dnd',
-		since: 91879201,
-		afk: false,
-	},
-
-	 */
-	intents: [Guilds,
+	lastShardId: 0,
+	shardCount: 1,
+	autoReconnect: true,
+	intents: [
+		Guilds,
 		GuildMembers,
 		MessageContent,
 		GuildMessages
@@ -42,35 +29,3 @@ const client = new Client({
 		VoiceStateUpdate
 	]
 });
-
-
-client.on('ready', () => {
-	console.log(`[CLIENT] The bot is running on ${client.guilds.size} servers.`);
-
-	client
-		.setActivity(
-			{
-				activities: [
-					{
-						name: 'Prodige',
-						type: 0,
-					},
-				],
-				status: 'dnd',
-				since: 91879201,
-				afk: false,
-			},
-			[0, 1]
-		)
-		.catch(console.error);
-});
-
-client.on('shardReady', (id) => {
-	console.log(`[SHARD] Shard(${id}) is now ready.`);
-});
-
-client.on('messageCreate', (m) => {
-	client.createMessage(m.channel_id, { content: 'Hello' });
-});
-
-client.login();
